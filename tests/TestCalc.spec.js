c02e23ec8ab85ae8e5b9e8a483d9399b58840331
Artem Konoval <kaedefromikeda@gmail.com>
2021-10-22 21:35:19 +0300

    Fix little errors

--- tests/TestCalc.spec.js
+++ tests/TestCalc.spec.js
@@ -3,13 +3,13 @@ const { expect } = require('chai');
 const Calculator = require('../app/calc.js');
 
 describe('Calculator', function () {
-    let calc = new Calculator;
+    const calc = new Calculator();
 
     it('should return sum of numbers', function () {
-        expect(calc.add(4, 5)).to.be.equal(9)
+        expect(calc.add(4, 5)).to.be.equal(9);
     });
 
     it('should return product of numbers', function () {
-        expect(calc.multiply(3, 4)).to.be.equal(12)
+        expect(calc.multiply(3, 4)).to.be.equal(12);
     });
 });
